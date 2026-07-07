f928f19b22fb7bcd0a6baf9d50d9254dcb2083d8
mohankumargupta <mohankumargupta@users.noreply.github.com>
2026-07-07 13:20:26 +1000

    added corfu to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,8 +61,67 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; syntax highlighting for python, rust et al
+;; need to install tree-sitter from source
+;; then compiled terminal emacs with:
+;; ./configure --without-x --without-dbus --without-gconf \
+;;             --without-gsettings --without-sound \
+;;             --without-cairo --without-lcms2 \
+;;             --without-jpeg --without-tiff \
+;;             --without-gif --without-png \
+;;             --without-rsvg --without-webp \
+;;             --without-imagemagick --without-xml2 \
+;;             --without-libsystemd --without-selinux \
+;;             --without-gpm --without-kerberos \
+;;             --without-kerberos5 --without-pop \
+;;             --without-hesiod --without-mail-unlink \
+;;             --disable-acl \
+;;             --disable-x \
+;;             --without-compress-install \
+;;             --with-tree-sitter
+
+
+
+
 (use-package treesit
   :ensure nil ; Built-in, no need to download
   :custom
   (treesit-auto-install-grammar 'always)
   (treesit-enabled-modes t))
+
+(use-package eglot-python-preset
+  :vc (:url "https://github.com/mwolson/eglot-python-preset"
+       :main-file "eglot-python-preset.el"))
+
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode)
+
+  :config
+  ;; Force Xref definitions to show in the Vertico minibuffer dropdown
+  (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
+
+  ;; Optional: Force Xref references to also use Vertico minibuffer
+  (setq xref-show-xrefs-function #'xref-show-definitions-completing-read)
+
+  ;; Optional: Tweak minibuffer behavior for a smoother workflow
+  (setq vertico-cycle t)           ; Cycle through the list seamlessly
+  (setq vertico-resize nil))       ; Grow/shrink the window dynamically
+
+
+(use-package corfu
+  :ensure t
+  :custom
+  (corfu-auto t)                 ;; Enable auto completion as you type
+  (corfu-cycle t)                ;; Enable cycling for keys like up/down/TAB
+  (corfu-auto-delay 0.2)         ;; Brief delay before popup shows up
+  (corfu-auto-prefix 2)          ;; Minimum number of characters to trigger auto-complete
+  (corfu-quit-at-boundary 'separator) ;; Never quit at completion boundary
+  :init
+  (global-corfu-mode)           ;; Turn on Corfu everywhere
+  ;; Direct Corfu-specific exact matching config
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides '((completion (styles basic)))))
+
+
